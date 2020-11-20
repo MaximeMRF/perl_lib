beaeb79006934db330d855f75f3fb80fabcc537a
Maxime <maxime.roth-fessler@epitech.eu>
2020-11-20 16:12:00 +0100

    add parsingPort

--- MyLib.pl
+++ MyLib.pl
@@ -2,28 +2,42 @@
          My Perl Lib
 =cut
 
-sub AddArray {
+sub addArray {
     foreach $item (@_) {
       $result += $item;
    }
    return $result;
 }
 
-sub MultArray {
+sub multArray {
     foreach $item (@_) {
       $result *= $item;
    }
    return $result;
 }
 
-sub Display {
+sub display {
     foreach $item (@_) {
       print("$item\n");
    }
 }
 
-sub ArgCount {
+sub argCount {
    return $i = @_;
+}
+
+sub listFile {
+    foreach $file (`ls`) {
+        print $file;
+    }
+}
+
+sub parsePort {
+    my ($flag, $arg) = split(/:/, @_[0]);
+    if ($flag eq "-port" || $flag eq "-p" && $arg > 0) {
+        return $arg;
+    }
+    return 0;
 }
 
 1;
